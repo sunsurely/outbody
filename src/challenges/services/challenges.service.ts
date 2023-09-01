a73a8cedf005242c982dda6e28cb12996fd99259
sangwoorhie <powercom92@naver.com>
2023-09-01 14:41:23 +0900

    fix.유저가 참가한 도전목록조회

--- src/challenges/services/challenges.service.ts
+++ src/challenges/services/challenges.service.ts
@@ -18,6 +18,7 @@ import { User } from 'src/users/entities/user.entity';
 import { DataSource } from 'typeorm';
 import { InviteChallengesRepository } from '../repositories/inviteChalleges.repository';
 import { Challenge } from '../entities/challenge.entity';
+import { Challenger } from '../entities/challenger.entity';
 
 @Injectable()
 export class ChallengesService {
@@ -411,7 +412,28 @@ export class ChallengesService {
   }
 
   // 유저 도전목록수 + 도전목록조회
-  async getUserChallenges(userId: number): Promise<[Challenge[], number]> {
-    return this.challengesRepository.getUserChallenges(userId);
+  async getUserChallenges(userId: number): Promise<Challenge[]> {
+    const userChallengerIds = await this.challengersRepository.find({
+      select: ['challengeId'],
+      where: { userId },
+    });
+
+    const userChallengeIds = userChallengerIds.map(
+      (challenger) => challenger.challengeId,
+    );
+
+    const userChallenges = await this.challengesRepository
+      .createQueryBuilder('challenge')
+      .whereInIds(userChallengeIds)
+      .select([
+        'challenge.title',
+        'challenge.startDate',
+        'challenge.endDate',
+        'challenge.challengeWeek',
+        'challenge.description',
+      ])
+      .getMany();
+
+    return userChallenges;
   }
 }
